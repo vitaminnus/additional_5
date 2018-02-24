d7eb94151612cec4fdb1a9de02b9c52f1e44a820
Viktor Dmitriev <vitaminnus@gmail.com>
2018-02-24 22:40:54 +0300

    Done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let array = str.split('');  
+  const stack = [];
+  let isOneBreak = array.some(element => {
+    const findConfig = bracketsConfig.find(elem => (element === elem[0] || element === elem[1]));
+    if (findConfig[0] !== findConfig[1]) {
+      if (element === findConfig[0]) {
+        stack.push(element);        
+      } else {
+        if (stack.pop() !== findConfig[0]) {
+          return true;
+        }
+      }
+    } else {
+      if (element === stack[stack.length-1]) {
+        stack.pop();
+      } else {
+        stack.push(element);
+      }
+    }
+  
+    return false;
+  });  
+  return (!stack.length && !isOneBreak );
 }
